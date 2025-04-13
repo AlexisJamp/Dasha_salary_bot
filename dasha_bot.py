f0d07c7739e13196014fca61885b846aad9dda21
Oleksii Manzia <154271476+AlexisJamp@users.noreply.github.com>
2025-04-13 18:40:05 +0300

    Update dasha_bot.py

--- dasha_bot.py
+++ dasha_bot.py
@@ -97,4 +97,5 @@ app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(handle_button))
 app.add_handler((MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message)))
 
-app.run_polling()
+if __name__ = '__main__':
+    app.run_polling()
